aa59e7226b39ffe0ec1060673d4f059d1232c335
Mahfuz <aamahfuz2@gmail.com>
2024-12-30 22:37:24 +0600

    Retrive command from rabitmq Queue

---
+++ Domain/Common/QueueCommandWrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Common
+{
+    public class QueueCommandWrapper
+    {
+        public string Body { get; set; } = string.Empty ;
+        public string CommandName { get; set; } = string.Empty ;
+        public string QueueName {  get; set; } = string.Empty ;
+    }
+}
