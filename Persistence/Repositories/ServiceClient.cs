d6185ffaf1a9d3fc3dd5c611e78678d0c46002dd
Mahfuz <aamahfuz2@gmail.com>
2025-01-04 22:59:15 +0600

    ServiceClient for rabitMq added

---
+++ Persistence/Repositories/ServiceClient.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Newtonsoft.Json;
+
+namespace Persistence.Repositories
+{
+    public class ServiceClient : IServiceClient
+    {
+        private readonly IRabbitMqService _rabbitMqService;
+
+        public ServiceClient(IRabbitMqService rabbitMqService)
+        {
+            _rabbitMqService = rabbitMqService;
+        }
+
+        public Response SendToQueue<Response>(string queueName, object payload) where Response : new()
+        {
+            var rabitMqMessage = new
+            {
+                Body = JsonConvert.SerializeObject(payload),
+                CommandName = payload.GetType().Name,
+                QueueName = queueName
+            };
+
+            _rabbitMqService.PublishToQueue(queueName, rabitMqMessage);
+
+            return new Response();
+        }
+    }
+}
