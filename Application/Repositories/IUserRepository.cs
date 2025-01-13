2d30af75b0f3c2fd999e4b2e6631bb8cda65ffb5
Mahfuz <aamahfuz2@gmail.com>
2025-01-13 13:44:44 +0600

    User Repository Created

---
+++ Application/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common;
+
+namespace Application.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<CommonResponse> CreateUserAsync();
+        Task<CommonResponse> UpdateUserAsync();
+        Task<CommonResponse> GetUsers();
+        Task<bool> IsUserExists(string email);
+    }
+}
---
+++ Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common;
+using Application.Repositories;
+using Domain.Entities;
+
+namespace Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IBaseRepository<User> _repository;
+
+        public UserRepository(IBaseRepository<User> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<CommonResponse> CreateUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CommonResponse> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsUserExists(string email)
+        {
+            var result = await _repository.FindOneAsync(x => x.Email == email);
+            return result is not null;
+        }
+
+        public Task<CommonResponse> UpdateUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
